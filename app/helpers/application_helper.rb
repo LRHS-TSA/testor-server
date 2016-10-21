7560e5f888dce2b8816b2b32d8cd983155955ccc
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-20 22:16:41 -0400

    Setup flashes using bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Base helper module
 module ApplicationHelper
+  # Gets the class based on flash key
+  def flash_class(level)
+    case level
+    when :notice then 'alert alert-info alert-dismissible fade in'
+    when :success then 'alert alert-success alert-dismissible fade in'
+    when :error then 'alert alert-error alert-dismissible fade in'
+    when :alert then 'alert alert-error alert-dismissible fade in'
+    end
+  end
 end
