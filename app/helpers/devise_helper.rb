ea4f8b7a88e62e67d806ac44732b20da82499c00
Christopher Miller <admin@mctherealm.net>
2016-10-23 18:27:28 -0400

    Changed location of error messages

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,7 +3,7 @@ module DeviseHelper
   def devise_error_messages!
     return '' unless devise_error_messages?
 
-    messages = safe_join resource.errors.full_messages.map { |msg| content_tag(:p, msg, class: 'm-b-0 m-t-1') }
+    messages = safe_join resource.errors.full_messages.map { |msg| content_tag(:p, msg, class: 'mb-0 mt-1') }
 
     html = content_tag :div, id: 'error_explanation', class: 'alert alert-danger', role: 'alert' do
       safe_join([content_tag(:h4, 'The following errors occured:', class: 'alert-heading m-b-1'), messages])
