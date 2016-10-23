2ce34656af0724fe9ae517157d61c88a77e3666e
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-23 19:57:12 -0400

    Add devise token auth
    TG-38 #closed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # User model (Using Devise)
 class User < ApplicationRecord
   devise :database_authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :lockable
+  include DeviseTokenAuth::Concerns::User
 end
