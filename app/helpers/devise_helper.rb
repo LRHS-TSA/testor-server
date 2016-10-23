60f71fe7704f2c5cf84d3d328e8d63104d320749
Christopher Miller <admin@mctherealm.net>
2016-10-23 16:38:25 -0400

    Added helper to override devise error message stub

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+# Helper functions for Devise Views
+module DeviseHelper
+  def devise_error_messages!
+    return '' unless devise_error_messages?
+
+    messages = safe_join resource.errors.full_messages.map { |msg| content_tag(:p, msg, class: 'm-b-0 m-t-1') }
+
+    html = content_tag :div, id: 'error_explanation', class: 'alert alert-danger', role: 'alert' do
+      safe_join([content_tag(:h4, 'Changes were not saved due to the following errors.', class: 'alert-heading m-b-1'), messages])
+    end
+
+    html
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+end
