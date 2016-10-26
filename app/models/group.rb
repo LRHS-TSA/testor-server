28028a2c789c0b7abe423dc36251eeeea59fe136
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-26 09:07:25 -0400

    Add group validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
+# Organizes students and teachers into groups
 class Group < ApplicationRecord
+  validates :name, presence: true, length: [minimum: 3, maximum: 16]
+  validates :description, presence: true
 end
