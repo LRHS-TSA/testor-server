ff7cf5d5414d9c24bfcbbe6f0db449e71be88e9f
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-26 09:14:27 -0400

    Add group description maximum

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 # Organizes students and teachers into groups
 class Group < ApplicationRecord
   validates :name, presence: true, length: [minimum: 3, maximum: 16]
-  validates :description, presence: true
+  validates :description, presence: true, length: [maximum: 2048]
 end
