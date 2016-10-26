4f079cf3040a1d62c648a34934edc6df24d9540e
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-26 09:31:25 -0400

    Group invalid tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Group, type: :model do
   it 'is valid with valid attributes' do
     expect(FactoryGirl.build(:group)).to be_valid
   end
+
+  it 'is invalid without a name' do
+    expect(FactoryGirl.build(:group, name: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a description' do
+    expect(FactoryGirl.build(:group, description: nil)).not_to be_valid
+  end
+
+  it 'is invalid with a name under 3 characters' do
+    expect(FactoryGirl.build(:group, name: 'aa')).not_to be_valid
+  end
+
+  it 'is invalid with a name over 16 characters' do
+    expect(FactoryGirl.build(:group, name: 'a' * 17)).not_to be_valid
+  end
+
+  it 'is invalid with a description over 2048 characters' do
+    expect(FactoryGirl.build(:group, description: 'a' * 2049)).not_to be_valid
+  end
 end
