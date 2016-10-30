2e44aefa6b6045b07de2134f569ad56317938310
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-30 18:40:32 -0400

    Revert "Add devise token auth"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 # User model (Using Devise)
 class User < ApplicationRecord
   devise :database_authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :lockable
-  include DeviseTokenAuth::Concerns::User
 end
