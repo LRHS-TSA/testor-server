46b7922d5b50aa383cfa99ca2c923656248deef9
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-30 19:23:41 -0400

    Add read tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,22 @@ RSpec.describe User, type: :model do
       FactoryGirl.create(:user, role: 1)
     end
 
+    it 'can read groups' do
+      is_expected.to be_able_to(:read, Group.new)
+    end
+
     it 'can create groups' do
       is_expected.to be_able_to(:create, Group.new)
+    end
+  end
+
+  context 'as a student' do
+    let(:user) do
+      FactoryGirl.create(:user, role: 0)
+    end
+
+    it 'can read groups' do
+      is_expected.to be_able_to(:read, Group.new)
     end
   end
 end
