b3401ebbcc31beebb0190e40ba2046aac7298820
Christopher Miller <admin@mctherealm.net>
2016-10-30 20:47:35 -0400

    Wrote group controller and added views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,43 @@
+# Controller for groups
 class GroupsController < ApplicationController
+  respond_to :html, :json
+  load_and_authorize_resource
+
+  def index
+    respond_with @groups
+  end
+
+  def show
+    respond_with @group
+  end
+
+  def new
+    respond_with @group
+  end
+
+  def edit
+    respond_with @group
+  end
+
+  def create
+    if @group.save
+      head status: :created, location: event_path(event)
+    else
+      render json: {error: event.errors}, status: :bad_request
+    end
+  end
+
+  def update
+    if @group.update_attributes(update_params)
+      head status: :ok
+    else
+      render json: {error: event.errors}, status: :bad_request
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:group).permit(:name, :description)
+  end
 end
