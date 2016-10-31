fbf1542b471c76c89357c32ef35b0af3262d3e6a
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-31 14:41:46 -0400

    Macros fix

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,5 @@
+require 'devise'
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend DeviseMacros, type: :controller
 end
--- spec/support/devise_macros.rb
+++ spec/support/devise_macros.rb
@@ -1,16 +1,24 @@
 # Macros to make rspec with devise easier
 module DeviseMacros
   def login_student
+    let(:user) do
+      FactoryGirl.create(:user, role: 0, name: 'student')
+    end
+
     before do
-      user = FactoryGirl.create(:user, role: 0, name: 'student')
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       user.confirm
       sign_in user
     end
   end
 
   def login_teacher
+    let(:user) do
+      FactoryGirl.create(:user, role: 1, name: 'teacher')
+    end
+
     before do
-      user = FactoryGirl.create(:user, role: 1, name: 'teacher')
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       user.confirm
       sign_in user
     end
