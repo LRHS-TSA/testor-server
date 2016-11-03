b3bed02826ec72e2bf6640b9f75671ee23b6c123
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-03 09:13:18 -0400

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102010702) do
+ActiveRecord::Schema.define(version: 20161103131226) do
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20161102010702) do
     t.integer "user_id"
     t.index ["group_id"], name: "index_members_on_group_id"
     t.index ["user_id"], name: "index_members_on_user_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
