380b82f523e6b64efa8d5133f5b83070d0fc7a32
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-03 09:25:53 -0400

    Test spec fixes

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Test, type: :model do
   end
 
   it 'is invalid without a name' do
-    expect(FactoryGirl.build(:test), name: nil).not_to be_valid
+    expect(FactoryGirl.build(:test, name: nil)).not_to be_valid
   end
 
   it 'is invalid with a name under 3 character' do
-    expect(FactoryGirl.build(:test), name: 'aa').not_to be_valid
+    expect(FactoryGirl.build(:test, name: 'aa')).not_to be_valid
   end
 
   it 'is invalid with a name over 32 characters' do
@@ -18,6 +18,6 @@ RSpec.describe Test, type: :model do
   end
 
   it 'is invalid wihout a user' do
-    expect(FactoryGirl.build(:test), user: nil).not_to be_valid
+    expect(FactoryGirl.build(:test, user: nil)).not_to be_valid
   end
 end
