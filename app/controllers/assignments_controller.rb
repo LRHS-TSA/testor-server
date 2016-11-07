65205e57bfee498cb5c7ec5643e73643f0ecb632
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-07 09:47:52 -0500

    AssignmentsController

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,52 @@
+# Controller for assignments
 class AssignmentsController < ApplicationController
+  load_and_authorize_resource :group
+  load_and_authorize_resource through: :group
+
+  def index
+    respond_with @assignments
+  end
+
+  def show
+    respond_with @assignment
+  end
+
+  def new
+    respond_with @assignment
+  end
+
+  def edit
+    respond_with @assignment
+  end
+
+  def create
+    if @assignment.save
+      head :created, location: group_assignment_path(@assignment.group, @assignment)
+    else
+      render json: {error: @assignment.errors}, status: :bad_request
+    end
+  end
+
+  def update
+    if @assignment.update_attributes(update_params)
+      head :ok
+    else
+      render json: {error: @assignment.errors}, status: :bad_request
+    end
+  end
+
+  def destroy
+    @assignment.destroy
+    head :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:assignment).permit(:name, :test_id, :start_date, :end_date, :time)
+  end
+
+  def update_params
+    params.require(:assignment).permit(:name, :test_id, :start_date, :end_date, :time)
+  end
 end
