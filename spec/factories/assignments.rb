47831243baa115006d9681ca0c92c3c5d57361c4
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-07 08:44:21 -0500

    rails g model Assignment

---
+++ spec/factories/assignments.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :assignment do
+    name "MyString"
+    group nil
+    test nil
+    start_date "2016-11-07 08:44:11"
+    end_date "2016-11-07 08:44:11"
+    length "2016-11-07 08:44:11"
+  end
+end
