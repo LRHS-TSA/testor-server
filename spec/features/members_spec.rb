45262381b8515fb8c02192cdf641ca8dfa9b2eea
Christopher Miller <admin@mctherealm.net>
2016-11-06 19:28:02 -0500

    Added test for kick button

--- spec/features/members_spec.rb
+++ spec/features/members_spec.rb
@@ -34,4 +34,29 @@ RSpec.feature 'Members' do
       end
     end
   end
+
+  context 'kicking a student', js: true do
+    login_teacher
+
+    let(:group) do
+      FactoryGirl.create(:group)
+    end
+
+    let(:other_user) do
+      FactoryGirl.create(:user, name: 'Test User')
+    end
+
+    background do
+      FactoryGirl.create(:member, group: group, user: user)
+      FactoryGirl.create(:member, group: group, user: other_user)
+    end
+
+    scenario 'with valid parameters' do
+      visit group_members_path(group)
+      last_member = group.members.last
+      click_button('kick_' + last_member.id.to_s)
+      visit group_members_path(group)
+      expect(page).to have_no_content(last_member.user.name)
+    end
+  end
 end
