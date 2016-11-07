c00cec84531adf9b32afb3be866ea318d193b6d3
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-07 08:51:30 -0500

    Assignment model

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(FactoryGirl.build(:assignment)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(FactoryGirl.build(:assignment, name: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a group' do
+    expect(FactoryGirl.build(:assignment, group: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a test' do
+    expect(FactoryGirl.build(:assignment, test: nil)).not_to be_valid
+  end
 end
