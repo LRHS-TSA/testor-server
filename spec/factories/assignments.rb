ef8f7be62a744d827997e2a451979d9a598b18df
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-07 08:49:50 -0500

    Assignments factory

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :assignment do
-    name "MyString"
-    group nil
-    test nil
-    start_date "2016-11-07 08:44:11"
-    end_date "2016-11-07 08:44:11"
-    length "2016-11-07 08:44:11"
+    sequence(:name) { |n| "Assignment #{n}" }
+    association :group
+    association :test
+    start_date nil
+    end_date nil
+    length nil
   end
 end
