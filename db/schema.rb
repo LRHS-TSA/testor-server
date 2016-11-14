e19915c058f79e0a948f8e088e7fbda5c8db11ee
Christopher Miller <admin@mctherealm.net>
2016-11-14 14:28:24 -0500

    Added question index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,83 +11,85 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161114135240) do
+
   create_table "assignments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "end_date"
-    t.integer "group_id"
-    t.time "length"
-    t.string "name"
+    t.string   "name"
+    t.integer  "group_id"
+    t.integer  "test_id"
     t.datetime "start_date"
-    t.integer "test_id"
+    t.datetime "end_date"
+    t.time     "length"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_assignments_on_group_id"
     t.index ["test_id"], name: "index_assignments_on_test_id"
   end
 
   create_table "groups", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.text "description"
-    t.string "name"
-    t.string "student_token"
-    t.string "teacher_token"
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.text     "description"
+    t.string   "name"
+    t.string   "student_token"
+    t.string   "teacher_token"
+    t.datetime "updated_at",    null: false
     t.index ["student_token"], name: "index_groups_on_student_token"
     t.index ["teacher_token"], name: "index_groups_on_teacher_token"
   end
 
   create_table "members", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.integer "group_id"
+    t.integer  "group_id"
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer  "user_id"
     t.index ["group_id"], name: "index_members_on_group_id"
     t.index ["user_id"], name: "index_members_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.integer "question_type"
-    t.integer "test_id"
-    t.text "text"
-    t.datetime "updated_at", null: false
+    t.integer  "test_id"
+    t.text     "text"
+    t.integer  "question_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
-    t.string "name"
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "authentication_token", limit: 30
+    t.string   "authentication_token",   limit: 30
     t.datetime "confirmation_sent_at"
-    t.string "confirmation_token"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
-    t.datetime "created_at", null: false
+    t.datetime "created_at",                                     null: false
     t.datetime "current_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.integer "failed_attempts", default: 0, null: false
+    t.string   "current_sign_in_ip"
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
+    t.integer  "failed_attempts",                   default: 0,  null: false
     t.datetime "last_sign_in_at"
-    t.string "last_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "locked_at"
-    t.string "name", null: false
+    t.string   "name",                                           null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
-    t.integer "role", default: 0, null: false
-    t.integer "sign_in_count", default: 0, null: false
-    t.string "unconfirmed_email"
-    t.string "unlock_token"
-    t.datetime "updated_at", null: false
+    t.string   "reset_password_token"
+    t.integer  "role",                              default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.string   "unconfirmed_email"
+    t.string   "unlock_token"
+    t.datetime "updated_at",                                     null: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
+
 end
