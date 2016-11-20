0e0942f5eb562b7b15db888e7f54500ea951094c
Christopher Miller <admin@mctherealm.net>
2016-11-20 16:10:17 -0500

    Added tests for creating and deleting multiple choice options

---
+++ spec/features/multiple_choice_options_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.feature 'Multiple Choice Options' do
+  login_teacher
+
+  context 'Creating a multiple choice option', js: true do
+    let(:test) do
+      FactoryGirl.create(:test, user: user)
+    end
+
+    background do
+      FactoryGirl.create(:question, test: test, question_type: 'multiple_choice')
+    end
+
+    scenario 'with valid parameters' do
+      visit test_questions_path(test)
+      params = FactoryGirl.build(:multiple_choice_option)
+      within('.add_option') do
+        fill_in 'Text', with: params.text
+      end
+      click_button 'Add'
+      expect(page).to have_content(params.text)
+    end
+
+    scenario 'with invalid parameters' do
+      visit test_questions_path(test)
+      click_button 'Add'
+      expect(page).to have_content('error')
+    end
+  end
+
+  context 'Deleting a multiple choice option', js: true do
+    let(:test) do
+      FactoryGirl.create(:test, user: user)
+    end
+
+    let(:question) do
+      FactoryGirl.create(:question, test: test, question_type: 'multiple_choice')
+    end
+
+    background do
+      FactoryGirl.create(:multiple_choice_option, question: question)
+    end
+
+    scenario 'with valid parameters' do
+      visit test_questions_path(test)
+      params = FactoryGirl.build(:multiple_choice_option)
+      click_button('×')
+      expect(page).not_to have_content(params.text)
+    end
+  end
+end
