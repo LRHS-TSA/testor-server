dad5b744cadde48d7e8217ee9e563b426faf35b4
Christopher Miller <admin@mctherealm.net>
2016-11-20 18:05:14 -0500

    Added multiple choice editing tests

--- spec/features/multiple_choice_options_spec.rb
+++ spec/features/multiple_choice_options_spec.rb
@@ -49,4 +49,40 @@ RSpec.feature 'Multiple Choice Options' do
       expect(page).not_to have_content(params.text)
     end
   end
+
+  context 'Editing a multiple choice option', js: true do
+    let(:test) do
+      FactoryGirl.create(:test, user: user)
+    end
+
+    let(:question) do
+      FactoryGirl.create(:question, test: test, question_type: 'multiple_choice')
+    end
+
+    background do
+      FactoryGirl.create(:multiple_choice_option, question: question)
+    end
+
+    scenario 'with valid parameters' do
+      visit test_questions_path(test)
+      find('.fa-pencil-square-o').click
+      params = FactoryGirl.build(:multiple_choice_option)
+      within('.edit_option') do
+        fill_in 'Text', with: params.text
+        click_button 'Save'
+      end
+      expect(page).to have_content(params.text)
+    end
+
+    scenario 'with invalid parameters' do
+      visit test_questions_path(test)
+      find('.fa-pencil-square-o').click
+      params = FactoryGirl.build(:multiple_choice_option, text: '')
+      within('.edit_option') do
+        fill_in 'Text', with: params.text
+        click_button 'Save'
+      end
+      expect(page).to have_content('error')
+    end
+  end
 end
