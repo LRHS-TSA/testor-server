40f277ed688dd46f4767a0b1e3c4153c7022ad73
Christopher Miller <admin@mctherealm.net>
2016-11-20 23:55:25 -0500

    Added matching pair editing tests

--- spec/features/matching_pairs_spec.rb
+++ spec/features/matching_pairs_spec.rb
@@ -29,4 +29,42 @@ RSpec.feature 'Matching Pair Options' do
       expect(page).to have_content('error')
     end
   end
+
+  context 'Editing a matching pair option', js: true do
+    let(:test) do
+      FactoryGirl.create(:test, user: user)
+    end
+
+    let(:question) do
+      FactoryGirl.create(:question, test: test, question_type: 'matching')
+    end
+
+    background do
+      FactoryGirl.create(:matching_pair, question: question)
+    end
+
+    scenario 'with valid parameters' do
+      visit test_questions_path(test)
+      find('.edit-pair-button').click
+      params = FactoryGirl.build(:matching_pair, item1: 'edit')
+      within('.edit_pair') do
+        fill_in 'Item One', with: params.item1
+        fill_in 'Item Two', with: params.item2
+        click_button 'Save'
+      end
+      expect(page).to have_content(params.item1)
+    end
+
+    scenario 'with invalid parameters' do
+      visit test_questions_path(test)
+      find('.edit-pair-button').click
+      params = FactoryGirl.build(:matching_pair, item1: '')
+      within('.edit_pair') do
+        fill_in 'Item One', with: params.item1
+        fill_in 'Item Two', with: params.item2
+        click_button 'Save'
+      end
+      expect(page).to have_content('error')
+    end
+  end
 end
