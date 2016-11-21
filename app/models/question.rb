1b5428ebee56738a15388e2ce77b44cdd2545de7
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-20 21:39:39 -0500

    Add matching as question type

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :test, inverse_of: :questions
   has_many :multiple_choice_options, inverse_of: :question
   has_many :matching_pairs, inverse_of: :question
-  enum question_type: [:essay, :multiple_choice, :short_answer]
+  enum question_type: [:essay, :multiple_choice, :short_answer, :matching]
 
   validates :test, presence: true
   validates :text, presence: true, length: {maximum: 4096}
