36c8f03b39b134e0910b8c4f1f945fae8d26ca6f
Connor Catlett <ccatlett2000@mctherealm.net>
2017-02-27 23:14:05 -0500

    Force item1 and item2 uniqueness for MatchingPairs

--- app/models/matching_pair.rb
+++ app/models/matching_pair.rb
@@ -3,6 +3,6 @@ class MatchingPair < ApplicationRecord
   belongs_to :question, inverse_of: :matching_pairs
 
   validates :question, presence: true
-  validates :item1, presence: true, length: {maximum: 64}
-  validates :item2, presence: true, length: {maximum: 64}
+  validates :item1, presence: true, length: {maximum: 64}, uniqueness: {scope: :question}
+  validates :item2, presence: true, length: {maximum: 64}, uniqueness: {scope: :question}
 end
--- spec/models/matching_pair_spec.rb
+++ spec/models/matching_pair_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe MatchingPair, type: :model do
   it 'is invalid with an item2 over 64 characters' do
     expect(FactoryGirl.build(:matching_pair, item2: 'a' * 65)).not_to be_valid
   end
+
+  it 'is invalid with a duplicate item1 and question' do
+    matching_pair = FactoryGirl.create(:matching_pair)
+    expect(FactoryGirl.build(:matching_pair, item1: matching_pair.item1, question: matching_pair.question)).not_to be_valid
+  end
+
+  it 'is invalid with a duplicate item2 and question' do
+    matching_pair = FactoryGirl.create(:matching_pair)
+    expect(FactoryGirl.build(:matching_pair, item2: matching_pair.item2, question: matching_pair.question)).not_to be_valid
+  end
 end
