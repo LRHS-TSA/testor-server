8a8131c5aa1108ce2969f113bcbc86a7545c30dd
Connor Catlett <ccatlett2000@mctherealm.net>
2017-03-03 01:55:45 -0500

    Add grade functions

--- app/models/session.rb
+++ app/models/session.rb
@@ -17,4 +17,32 @@ class Session < ApplicationRecord
   def locked?
     !assignment.start_date.nil? && assignment.start_date.utc > Time.now.utc || !assignment.end_date.nil? && assignment.end_date.utc < Time.now.utc || !assignment.length.nil? && !start_time.nil? && assignment.length.seconds.ago > start_time
   end
+
+  def grade_multiple_choice
+    assignment.test.questions.where(question_type: :multiple_choice).find_each do |question|
+      next unless Score.find_by(session: self, question: question).nil?
+      answer = question.multiple_choice_answers.find_by(session: self)
+      if answer.nil? || !answer.multiple_choice_option.correct
+        Score.create(session: self, question: question, score: 0)
+      else
+        Score.create(session: self, question: question, score: question.points)
+      end
+    end
+  end
+
+  def grade_matching
+    assignment.test.questions.where(question_type: :matching).find_each do |question|
+      next unless Score.find_by(session: self, question: question).nil?
+      answers = question.matching_pair_answers.where(session: self)
+      correct_count = 0
+      answers.find_each do |answer|
+        correct_count += 1 unless question.matching_pairs.find_by(item1: answer.item1, item2: answer.item2).nil?
+      end
+      if correct_count == question.matching_pairs.length
+        Score.create(session: self, question: question, score: question.points)
+      else
+        Score.create(session: self, question: question, score: 0)
+      end
+    end
+  end
 end
